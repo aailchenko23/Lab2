b3fc4a9d3203e4000356a63f84fc4d02ec0196dc
aailchenko23 <125138023+aailchenko23@users.noreply.github.com>
2023-03-25 21:43:30 +0200

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,14 @@
+package Lab2;
+
+import static java.lang.Math.max;
+import static java.lang.Math.toIntExact;
+
+public class Third {
+    public static void main(String[]args){
+        double  a;
+          a = 6577.56736;
+          int round;
+          a = (int) (a + 0.5);
+        System.out.println(+a);
+    }
+}
